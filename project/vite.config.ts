48c76148b0f04579e9905bafb6e81f4f30a5f015
GOLDENSPIRALCRISPYDUCK <2247553529@qq.com>
2025-04-17 02:40:53 +0800

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -5,13 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react', 'xlsx'], // 同时排除 xlsx
+    exclude: ['lucide-react'],
   },
   assetsInclude: ['**/*.xlsx'], // 将 .xlsx 文件视为静态资源
   build: {
     assetsInlineLimit: 0, // 确保 Excel 文件不会被内联为 base64
-    rollupOptions: {
-      external: ['xlsx'], // 关键：将 xlsx 标记为外部依赖
-    },
   },
 });
